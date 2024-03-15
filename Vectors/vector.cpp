fcdecd3edac87f56cc4e2f9de9991c008766667d
Future Mulenga <mulengafuture14@gmail.com>
2024-03-15 11:20:16 -1000

    second push to the repo

--- Vectors/vector.cpp
+++ Vectors/vector.cpp
@@ -1,7 +1,28 @@
+/*PROGRAM: FUTURE MULENGA
+PURPOSE: LEARNING VECTORS
+*/ 
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main (){
+       
+    // Declare a vector of integers with size
+    vector <int> numbers(10);
+    int value;
 
-     
+    for(int x=0; x<10; x++){
+
+         cout << "Enter number "<<endl;
+         cin >> value;
+         numbers.push_back(value);
+    }
+
+    // Display the elements in the vector   
+    for(int y=0; y < numbers.size(); y ++) {
+
+        cout << numbers[y]<< " "<<endl;
+    }
+
 }
