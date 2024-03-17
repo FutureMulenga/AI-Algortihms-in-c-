dc123482a69301aff59e88db861b8fdfc3c1e9a2
Future Mulenga <mulengafuture14@gmail.com>
2024-03-17 01:32:45 -1000

    adding lists and puting programs in their repective folders

---
+++ Lists/List.cpp
@@ -0,0 +1,15 @@
+/*
+   DEVELOPER: FUTURE MULENGA 
+   DATE CREATED: 17-12-2024
+   PURPOSE: LEARNING LISTS IN C++
+*/
+
+#include <iostream>
+#include <list>
+using namespace std;
+
+
+int main(){
+
+      
+}
