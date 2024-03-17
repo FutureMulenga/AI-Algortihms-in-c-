4906eb746907bd4abd98bfbc247e471cee2885d4
Future Mulenga <mulengafuture14@gmail.com>
2024-03-17 03:32:30 -1000

    implementing the algorithm

--- BreadthFirstAlgorith/program.cpp
+++ BreadthFirstAlgorith/program.cpp
@@ -25,7 +25,7 @@ class Graph{
             //this is the constructor               
             Graph(int v);
 
-            // This  function is used to add an edge to the 
+            // This  function is used to add an edge to the graph 
             void  addEdge(int v, int w);
 
             //This is the method to traverse through the graph
@@ -46,4 +46,48 @@ void Graph::addEdge(int v, int w){
     //Adds the element at the end of the list
     adj[v].push_back(w); 
 }
+
+//Perform BFs given a starting vertex 
+void Graph::BFS(int s){
+
+    //Start with vertices as not visited 
+    bool *visited = new bool[V];
+
+    for(int i=0; i<V; i++){
+        visited[i] = false;
+    }
+
+    //Creates a queue for BFS
+    list<int> queue;
+
+    //Start vertex marked as visited and added to queue
+    visited[s] = true;
+    queue.push_back(s);
+
+
+    //Continue untill the queue is empty
+    while (!queue.empty()){
+
+    //Getting the front of the queue and remove it 
+         s = queue.front();
+         queue.pop_front();
+
+         //Get all adjecement vertices from that vertex 
+         cout << "checking vertices for vertex "<<s<<endl;
+
+         for(auto i = adj[s].begin(); i != adj[s].end(); i++){
+             
+             //check the nodes not visited yet 
+             if(!visited[*i]){
+                 cout << "visited and queued "<<*i<<endl;
+                 visited[*i] = true;
+             }
+         }
+
+    }
+    
+
+}
+
+
 
