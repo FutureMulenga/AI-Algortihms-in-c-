92ef99ce1da8a3d5b40b3e1aa6a169b9fc398e3d
Future Mulenga <mulengafuture14@gmail.com>
2024-03-17 02:04:48 -1000

    declaring the iterator

--- Lists/List.cpp
+++ Lists/List.cpp
@@ -2,6 +2,7 @@
    DEVELOPER: FUTURE MULENGA 
    DATE CREATED: 17-12-2024
    PURPOSE: LEARNING LISTS IN C++
+   GITHUB: https://github.com/FutureMulenga/AI-Algortihms-in-c-/tree/main/Lists
 */
 
 #include <iostream>
@@ -11,5 +12,22 @@ using namespace std;
 
 int main(){
 
-      
+   // Declare a list of strings
+   list<string> names;
+
+// Adding elements to the list  using push_back() method and push_front()  method
+   names.push_back("FUTURE");
+   names.push_front("MULENGA");
+   names.push_back("CHANDA");
+
+   //declare the iterator 
+   list<string>:: iterator  itr;
+   itr = names.begin();
+
+//loop to iterate through the elememts in the lists 
+   for(itr; itr != names.end() ; ++itr) {
+
+      cout<<*itr<<endl;
+   }
+
 }
